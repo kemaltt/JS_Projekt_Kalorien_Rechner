bfcfdbe1d289c7e05067e02c417ca9adf6bba64d
kemaltt <ttnckemal@gmail.com>
2022-05-23 21:49:43 +0200

    last commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,17 +12,23 @@ const output_gesamt_kcal = document.getElementById('output_gesamt_kcal')
 const output_gesamt_kJ = document.getElementById('output_gesamt_kJ')
 
 const calculate = () => {
-    let grundUmsatz, grundUmsatzkj, gesamtUmsatz, gesamtUmsatzkj;
-    if (female.checked) {
-        grundUmsatz = 665.1 + (9.6 * weight.value) + (1.8 * size.value) - (4.7 * alter.value);
+
+    if (size.value == '' || alter.value == '' || weight.value == '') {
+        alert('Geben Sie eine Körpergröße,Alter oder Gewicht')
     } else {
-        grundUmsatz = 664.7 + (13.7 * weight.value) + (5 * size.value) - (6.8 * alter.value);
+        let grundUmsatz, grundUmsatzkj, gesamtUmsatz, gesamtUmsatzkj;
+        if (female.checked) {
+            grundUmsatz = 665.1 + (9.6 * weight.value) + (1.8 * size.value) - (4.7 * alter.value);
+        } else {
+            grundUmsatz = 664.7 + (13.7 * weight.value) + (5 * size.value) - (6.8 * alter.value);
+        }
+        output_grund_kcal.innerHTML = (grundUmsatz / 1000).toFixed(3);
+        gesamtUmsatz = grundUmsatz * Number(palFaktor.value);
+        output_gesamt_kcal.innerHTML = (gesamtUmsatz / 1000).toFixed(3);
+        grundUmsatzkj = grundUmsatz * 4.1868;
+        output_grund_kJ.innerHTML = (grundUmsatzkj / 1000).toFixed(3);
+        gesamtUmsatzkj = gesamtUmsatz * 4.1868;
+        output_gesamt_kJ.innerHTML = (gesamtUmsatzkj / 1000).toFixed(3)
     }
-    output_grund_kcal.innerHTML = (grundUmsatz / 1000).toFixed(3);
-    gesamtUmsatz = grundUmsatz * Number(palFaktor.value);
-    output_gesamt_kcal.innerHTML = (gesamtUmsatz / 1000).toFixed(3);
-    grundUmsatzkj = grundUmsatz * 4.1868;
-    output_grund_kJ.innerHTML = (grundUmsatzkj / 1000).toFixed(3);
-    gesamtUmsatzkj = gesamtUmsatz * 4.1868;
-    output_gesamt_kJ.innerHTML = (gesamtUmsatzkj / 1000).toFixed(3)
+
 }
